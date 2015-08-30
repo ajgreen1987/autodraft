f4106d9f99a52c242244ea3e73c96e20781827d9
Green <dzqbdf@Greens-MacBook.local>
2015-08-30 16:48:24 -0400

    - Adding background thread

--- AutoDraft/GMSAllDraftViewController.m
+++ AutoDraft/GMSAllDraftViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "GMSAllDraftViewController.h"
+#import "DGActivityIndicatorView.h"
 
 
 @interface GMSAllDraftViewController ()
+
+@property (nonatomic, strong) DGActivityIndicatorView *activityIndicatorView;
 
 @end
 
@@ -17,13 +20,34 @@
 
 - (void) viewDidAppear:(BOOL)animated
 {
+
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
     
-    self.arrayToUse = [[GMSDraftManager sharedInstance] allDraftObjects];
+    //Start an activity indicator here
+    GMSAllDraftViewController __weak *threadSafeSelf = self;
     
-    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        //Call your function or whatever work that needs to be done
+        //Code in this part is run on a background thread
+        threadSafeSelf.arrayToUse = [[GMSDraftManager sharedInstance] allDraftObjects];
+
+        
+        dispatch_async(dispatch_get_main_queue(), ^(void) {
+            
+            //Stop your activity indicator or anything else with the GUI
+            //Code here is run on the main thread
+            [threadSafeSelf.tableView reloadData];
+            
+            [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+            
+        });
+    });
     
-    [self.tableView reloadData];
+    
+
+    
+    [self stopAndRemoveActivityIndicator];
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
@@ -42,6 +66,20 @@
         // Delete the row from the data source
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     }
+}
+
+- (void) setupAndStartActivityIndicator
+{
+    self.activityIndicatorView= [[DGActivityIndicatorView alloc] initWithType:DGActivityIndicatorAnimationTypeDoubleBounce tintColor:[UIColor blackColor] size:20.0f];
+    self.activityIndicatorView.frame = CGRectMake(0.0f, 0.0f, 50.0f, 50.0f);
+    
+    [self.activityIndicatorView startAnimating];
+}
+
+- (void) stopAndRemoveActivityIndicator
+{
+    [self.activityIndicatorView stopAnimating];
+    [self.activityIndicatorView removeFromSuperview];
 }
 
 @end
